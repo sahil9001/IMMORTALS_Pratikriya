15bec083534c2381e1a163b18b3bae7070d9dc7d
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2022-02-26 16:50:21 +0530

    header updated

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -17,8 +17,8 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <Layout>
         <SEO
-          title="Startup Agency Alpha Landing"
-          description="Collection of free top of the line startup landing templates built using react/ next js. Free to download, simply edit and deploy! Updated weekly!"
+          title="Pratikriya"
+          description="Audio Sentiment Analysis"
         />
         <Banner />
         <Support />
