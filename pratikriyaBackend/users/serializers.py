135d3bf96e55560abecc65aeb673026979b03d44
Prakhar Asaiya <63089922+prakhar-asaiya@users.noreply.github.com>
2022-02-26 16:43:41 +0530

    Added logo

--- pratikriyaBackend/users/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(allow_blank=False)
-    password = serializers.CharField(allow_blank=False)
--- pratikriyaBackend/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from ..users import views
-
-urlpatterns = [
-    path('login/', views.LoginAPIView.as_view(), name="sign_in"),
-]
