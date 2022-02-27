2535cdbf610d49539394051abdb937f2d57710ee
Siddharth Mishra <smishra1605@gmail.com>
2022-02-27 12:12:23 +0530

    dashBoard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -5,10 +5,7 @@ import { Box, Flex } from "theme-ui";
 import "../components/dashboard/data.js";
 
 const Dashboard = () => {
-  const [id, setId] = useState(null);
-  const setval = () => {
-    setId(id);
-  }
+  const [caller, setCaller] = useState(null);
   return (
     <Box>
       <Flex
@@ -20,12 +17,20 @@ const Dashboard = () => {
         }}
       >
         <Box sx={{ maxWidth: "500px" }}>
-          <AudioList setId={setId} id={id} />
+          <AudioList setCaller={setCaller} />
         </Box>
         <Box sx={{ marginRight: "700px", marginTop: "200px" }}>
           Prediction Result: {
             <Box>
-            {id? ("jello"):(id)}
+              {caller ? (<div>
+                <div className='label'>Caller Full Name : </div><div className='label-value'>{caller.fullName}</div>
+                <div className='label'>Phone Number :</div><div className='label-value'>{caller.phoneNumber}</div>
+                <div className='label'>Location :</div><div className='label-value'>{caller.location}</div>
+                <div className='label'>Call Time Stamp :</div><div className='label-value'>{caller.timeStamp}</div>
+                <div className='label'>Call Duration :</div><div className='label-value'>{caller.callDuration}</div>
+                <div className='label'>Call Purpose :</div><div className='label-value'>{caller.callPurpose}</div>
+                <div className='label'>Final Prediction :</div><div className='label-value'>{caller.prediction}</div>
+              </div>) : ("")}
             </Box>
           }
         </Box>
